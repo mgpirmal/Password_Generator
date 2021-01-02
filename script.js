4ec596d2dcc0bf4ea2928cccfbd457721a20f3ca
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-01-01 19:24:54 -0500

    Solved the issue with first While loop and added .splice to clear array when done

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 var lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var special = ["!","@","#","$","%","^","&","*","(",")","<",">","?"];
 var num = [1,2,3,4,5,6,7,8,9,0];
-var size;
-var upper;
-var specialChar;
-var aNum;
-var i=0;
-var finalPassword =[]
+let size;
+let upper;
+let specialChar;
+let aNum;
+let finalPassword = [];
+
 // Write password to the #password input
 function writePassword() {
   // Ask for size and password content
 size = prompt("How long do you want your password?");
+      while (size < 8 || size > 128) {
+        alert("Password must be between 8 to 128 characters.");
+        size = prompt("How long do you want your password?");
+      }
 upper = confirm("Do you want uppercase characters?");
 specialChar = confirm("Do you want special characters?");
 anum = confirm("Do you want numbers in your password?");
+let i=0;
 
 // concat arrays based on prefrences 
 // if statements that will run based on prefrences
@@ -48,15 +53,17 @@ if (upper == false && specialChar == false && anum == false){
 
 // Take the new concat array and pull a random set of characters from it
 //Run the loop based on the 'size' of password wanted from user
-while (i < size){
-password = newpass[Math.floor(Math.random() * newpass.length)];
+for (i = 0; i < size; i++){
+let password = newpass[Math.floor(Math.random() * newpass.length)];
 finalPassword.push(password);
 console.log(finalPassword);
-i++;
 }
 //Take the final array and replace the commas with spaces(join method) and then use the trim method to remove the empty space
 alert("Your Password is " + finalPassword.join("").trim());
 
+//Ran into issue with finalPassword array saving previous button click answer.
+// Added splice method to clear array if user decides to click again without refreshing page.
+finalPassword.splice(0,finalPassword.length);
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
